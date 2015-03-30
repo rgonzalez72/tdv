ff80196024798f08be26657b923b743a9a38e373
Rodolfo <rgonzalez72@yahoo.com>
2015-03-30 18:12:57 +0100

    aa

--- tdgui.py
+++ tdgui.py
@@ -20,12 +20,53 @@ class TDGUI (wx.Frame):
         Qitem = filem.Append (wx.ID_EXIT, "&Quit", "Quit application")
         menubar.Append (filem, "&File")
         menubar.Append (helpm, "&Help")
+        Aitem = helpm.Append (wx.ID_HELP, "&About", "About")
 
         self.SetMenuBar (menubar)
 
         self.Bind (wx.EVT_MENU, self.OnQuit, Qitem)
+        self.Bind (wx.EVT_MENU, self.OnAbout, Aitem)
 
     def OnQuit (self, e):
+        self.Close ()
+
+    def OnAbout (self, e):
+        A = AboutDialog (self, wx.ID_ANY, "About TDV") 
+        A.Centre ()
+        A.ShowModal ()
+
+class AboutDialog (wx.Dialog):
+    def __init__ (self, parent, id, title):
+        wx.Dialog.__init__ (self, parent, id, title)
+
+        vbox = wx.BoxSizer (wx.VERTICAL)
+        hbox0 = wx.BoxSizer (wx.HORIZONTAL)
+        hbox1 = wx.BoxSizer (wx.HORIZONTAL)
+        hbox2 = wx.BoxSizer (wx.HORIZONTAL)
+        hbox3 = wx.BoxSizer (wx.HORIZONTAL)
+
+        panel = wx.Panel (self, wx.ID_ANY)
+
+        label1 = wx.StaticText (panel, wx.ID_ANY, 
+                "An application for visualizing time doctor files.")
+        hbox1.Add (label1, 0, wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, 20)
+
+        self.btnClose = wx.Button (panel, wx.ID_CLOSE, "&Close")
+        hbox3.Add (self.btnClose, 0, wx.CENTER, 20)
+        self.Bind (wx.EVT_BUTTON, self.OnClose, self.btnClose)
+
+        vbox.Add (hbox0, 0, wx.EXPAND)
+        vbox.Add (hbox1, 0, wx.LEFT)
+        vbox.Add (hbox2, 0, wx.LEFT)
+        vbox.Add (hbox3, 0, wx.CENTER)
+
+        panel.SetSizer(vbox)
+        minSize = vbox.GetMinSize()
+        height = minSize.GetHeight ()
+        minSize.SetHeight (height + 60)
+        self.SetSize (minSize )
+
+    def OnClose (self, e):
         self.Close ()
 
 app = wx.App ()
